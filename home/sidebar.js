15dcc89198cfe31c7911cc42da01925c0b21f55e
TheMiranha <lxbrmiranda@gmail.com>
2022-05-30 01:53:20 -0300

    first commit

---
+++ home/sidebar.js
@@ -0,0 +1,10 @@
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+
+const Item = () => {
+    return {
+        title: "TikTok",
+        icon: <SmartphoneIcon style={{marginRight: 10}}/>
+    }
+}
+
+export default { Item }
